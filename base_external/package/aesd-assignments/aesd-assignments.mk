388d9d19d29a6658b21a963b1b492776efeb6710
rishap <rishapsingh704@gmail.com>
2024-11-14 12:22:05 +0530

    updated the ssh commit ID try2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '607d88216aecee0d6178b0cb2729ae0f7e024391'
+AESD_ASSIGNMENTS_VERSION = 'c26febdb2b1c97f3815d3f8263acd13349ea02b3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
